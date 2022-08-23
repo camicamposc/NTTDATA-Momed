34ac0bc8c49855de8e54ed42084bffc847a0b9df
Heike Tineo <100814387+Htineo@users.noreply.github.com>
2022-08-23 15:47:40 -0500

    Revert "subiendo template con nuevo diseño para las vistas"

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,12 +1,9 @@
 import NotView from "../components/not-view/NotView";
-import PageTemplate from "./Template";
 
 const Dashboard = () => {
   return (
     <>
-    <PageTemplate>
       <h1>Dashboard</h1>
-    </PageTemplate>
       <NotView />
     </>
   );
--- src/routes/Profesionales.jsx
+++ src/routes/Profesionales.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useContext, useEffect } from "react";
-import { DataContext } from "../context/DataContext";
-import Visor from "../components/Visor/Visor";
-import PageTemplate from "./Template";
-// import PageTemplate from "../components/template/template";
+import React, { useState, useContext, useEffect } from 'react'
+import { DataContext } from '../context/DataContext';
+import Visor from '../components/Visor/Visor'
+
 
 const Profesionales = () => {
+
   const { getProfessionals, getProcess } = useContext(DataContext);
+
 
   useEffect(() => {
     getProcess();
     getProfessionals();
   }, []);
 
+
   return (
     <>
-      <PageTemplate> 
-        <Visor></Visor>
-      </PageTemplate>
+      <Visor />
     </>
   );
 };
